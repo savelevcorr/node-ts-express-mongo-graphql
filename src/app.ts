2c1320c8fa3166c8929459cf1c643e8ab6195ecb
Andrew Savelev <savelevcorr@gmail.com>
2020-04-29 11:56:58 +0300

    redirecting request

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,36 @@
-import http, { IncomingMessage, ServerResponse } from 'http';
+import http, { IncomingMessage, ServerResponse } from "http";
+import fs from "fs";
 
 function rqListener(req: IncomingMessage, res: ServerResponse): void {
-    const {url} = req;
+  const { url, method } = req;
 
-    if (url === '/') {
-        res.setHeader('Contnet-Type', 'text/html');
-        res.write('<html>')
-        res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body>
+  if (url === "/") {
+    res.setHeader("Contnet-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`<body>
             <form action="/message" method="POST">
                 <input type="text" autocomplete="none" name="message" />
                 <button type="submit">Send message</button>
             </form>
         </body>`);
-        res.write('</html>');
-        return res.end();
-    }
-    
-    res.setHeader('Contnet-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My First page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server</h1></body>');
-    res.write('</html>');
-    res.end();
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message/message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Contnet-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
 }
 
 const server = http.createServer(rqListener);
