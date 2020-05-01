a01118d57ce53a3b0e52abcb4ef4b74ed5f3418a
Andrew Savelev <savelevcorr@gmail.com>
2020-05-01 23:26:06 +0300

    Split files and add launch settings

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,6 @@
 import http, { IncomingMessage, ServerResponse } from "http";
-import fs from "fs";
 
-function rqListener(req: IncomingMessage, res: ServerResponse): void {
-  const { url, method } = req;
-  let body: any[] = [];
-  let parsedBodu: string;
-  let message: string;
-
-  if (url === "/") {
-    res.setHeader("Contnet-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(`<body>
-            <form action="/message" method="POST">
-                <input type="text" autocomplete="none" name="message" />
-                <button type="submit">Send message</button>
-            </form>
-        </body>`);
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    req.on("data", (data) => {
-      body.push(data);
-    });
-    req.on("end", () => {
-      parsedBodu = Buffer.concat(body).toString();
-      message = parsedBodu.split("=")[1];
-
-      fs.writeFile("message.txt", message, (error) => {
-        if (!error) {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          return res.end();
-        }
-      });
-    });
-  }
-
-  res.setHeader("Contnet-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First page</title></head>");
-  res.write("<body><h1>Hello from my Node.js server</h1></body>");
-  res.write("</html>");
-  res.end();
-}
+import rqListener from "./routes";
 
 const server = http.createServer(rqListener);
 server.listen(3001);
---
+++ src/routes.ts
@@ -0,0 +1,57 @@
+import { IncomingMessage, ServerResponse } from "http";
+import fs from "fs";
+
+/**
+ * Simple routes handler
+ * It allows users to send a message and store its content on the server in a simple text file.
+ * @param req {any}
+ * @param res {object}
+ * @returns {void}
+ */
+function rqListener(req: IncomingMessage, res: ServerResponse): void {
+  const { url, method } = req;
+  let body: any[] = [];
+  let parsedBodu: string;
+  let message: string;
+
+  if (url === "/") {
+    res.setHeader("Contnet-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`<body>
+              <form action="/message" method="POST">
+                  <input type="text" autocomplete="none" name="message" />
+                  <button type="submit">Send message</button>
+              </form>
+          </body>`);
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    req.on("data", (data) => {
+      body.push(data);
+    });
+    req.on("end", () => {
+      parsedBodu = Buffer.concat(body).toString();
+      message = parsedBodu.split("=")[1];
+
+      fs.writeFile("message.txt", message, (error) => {
+        if (!error) {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          return res.end();
+        }
+      });
+    });
+  }
+
+  res.setHeader("Contnet-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
+}
+
+export default rqListener;
