a71abdbbdbaf69c64c01f1773e3c8739ac7bb8e4
Andrew Savelev <savelevcorr@gmail.com>
2020-06-11 15:01:08 +0300

    An example of how to handle diffrent routes

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.use((res, req, next) => {
-  console.log('In the middleware!');
-  next();
-});
+// All middlewares are executes from top to bottom
 
-app.use((req, res, next) =>{
+// We are don't call the next function, 
+// so we don't go to the next middleware
+app.use('/add-product', (_, res) => {
+  res.send('<h1>The "Add Product" page</h1>')
+})
+
+app.use('/', (_, res) =>{
   console.log('In an another middleware');
   res.send('<h1>Middleware header</h1>');
 })
