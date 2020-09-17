7a49997ab5f4c0234b4dcc29a3a3462327a39f96
Andrey Savelev <savelevcorr@gmail.com>
2020-09-17 16:26:49 +0300

    Add 404 route handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, {Response} from 'express';
 import bodyParser from 'body-parser';
 
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
+import { join } from "path";
+import rootDir from "./utils/path";
 
 const app = express();
 
@@ -18,9 +20,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // 404 route
-app.use((_, res) => {
+app.use((_, res: Response) => {
   res.status(404)
-  .send('<h1>Page Not Found</h1>');
+  .sendFile(join(rootDir, 'views', '404.html'));
 });
 
- app.listen(3001);
+app.listen(3001);
