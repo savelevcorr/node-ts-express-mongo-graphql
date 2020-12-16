6d5eeee76058f6f68137ccb204e31a019876811d
Andrey Savelev <savelevcorr@gmail.com>
2020-12-16 09:55:02 +0300

    Update shop rout

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,14 +1,12 @@
-import {join} from 'path';
 import express from 'express';
 
-import rootDir from '../utils/path';
 import {products} from "./admin";
 
 const router = express.Router();
 
 // Root route
 router.get('/', (_, res) =>{
-  res.render('shop')
+  res.render('shop', {products, docTitle: 'Shop'})
 })
 
 export default router;
